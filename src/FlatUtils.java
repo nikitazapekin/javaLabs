a3d8313e51efda364f1915758aeb1e90fd5ecfa3
nikitazapekin <wotblitz362@mail.ru>
2025-02-18 18:28:44 +0300

    last

--- src/FlatUtils.java
+++ src/FlatUtils.java
@@ -1,4 +1,38 @@
-import java.util.Arrays;
+public class FlatUtils {
+
+    public static void printFlatsByRooms(Flat[] flats, int roomCount) {
+        System.out.println("Квартиры с " + roomCount + " комнатами:");
+        for (int i = 0; i < flats.length; i++) {
+            if (flats[i] != null && flats[i].getRooms() == roomCount) {
+                System.out.println(flats[i]);
+            }
+        }
+    }
+
+
+    public static void printFlatsByRoomsAndFloor(Flat[] flats, int roomCount, int minFloor, int maxFloor) {
+        System.out.println("Квартиры с " + roomCount + " комнатами, этаж от " + minFloor + " до " + maxFloor + ":");
+        for (int i = 0; i < flats.length; i++) {
+            if (flats[i] != null && flats[i].getRooms() == roomCount &&
+                    flats[i].getFloor() >= minFloor && flats[i].getFloor() <= maxFloor) {
+                System.out.println(flats[i]);
+            }
+        }
+    }
+
+
+    public static void printFlatsByArea(Flat[] flats, double minArea) {
+        System.out.println("Квартиры с площадью больше " + minArea + " м²:");
+        for (int i = 0; i < flats.length; i++) {
+            if (flats[i] != null && flats[i].getArea() > minArea) {
+                System.out.println(flats[i]);
+            }
+        }
+    }
+}
+
+
+/*import java.util.Arrays;
 
 public class FlatUtils {
 
@@ -23,3 +57,4 @@ public class FlatUtils {
                 .forEach(System.out::println);
     }
 }
+*/
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Flat[] flats = {
+
+        Flat[] flats = new Flat[5];
+
+        flats[0] = new Flat(1, 101, 55.5, 3, 2, "Ленина 10", "Панельный", 50);
+        flats[1] = new Flat(2, 102, 72.3, 5, 3, "Пушкина 20", "Кирпичный", 60);
+        flats[2] = new Flat(3, 103, 60.0, 2, 2, "Гагарина 5", "Монолит", 70);
+        flats[3] = new Flat(4, 104, 90.1, 7, 4, "Советская 30", "Панельный", 40);
+        flats[4] = new Flat(5, 105, 45.0, 1, 1, "Октябрьская 15", "Кирпичный", 80);
+
+
+        FlatUtils.printFlatsByRooms(flats, 2);
+        FlatUtils.printFlatsByRoomsAndFloor(flats, 3, 3, 6);
+        FlatUtils.printFlatsByArea(flats, 50);
+
+      /*  Flat[] flats = {
                 new Flat(1, 101, 55.5, 3, 2, "Ленина 10", "Панельный", 50),
                 new Flat(2, 102, 72.3, 5, 3, "Пушкина 20", "Кирпичный", 60),
                 new Flat(3, 103, 60.0, 2, 2, "Гагарина 5", "Монолит", 70),
@@ -14,7 +28,7 @@ public class Main {
         FlatUtils.printFlatsByRooms(flats, 2);
         FlatUtils.printFlatsByRoomsAndFloor(flats, 3, 3, 6);
         FlatUtils.printFlatsByArea(flats, 50);
-
+*/
 
     }
 }
