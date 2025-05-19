4bb5e3b4f437ff496034c246c2135f2e81565808
nikitazapekin <wotblitz362@mail.ru>
2025-05-19 14:25:24 +0300

    last

--- src/main/java/org/example/UserSelectors.java
+++ src/main/java/org/example/UserSelectors.java
@@ -7,7 +7,43 @@ import java.util.Comparator;
 import java.util.List;
 
 public class UserSelectors {
-    public static final UserSelector OLDEST = users -> users.stream()
+
+
+
+    public static final UserSelector OLDEST = users -> {
+        User oldest = null;
+        for (User user : users)
+            if (oldest == null || user.getAge() > oldest.getAge())
+                oldest = user;
+        return oldest;
+    };
+
+    public static final UserSelector YOUNGEST = users -> {
+        User youngest = null;
+        for (User user : users)
+            if (youngest == null || user.getAge() < youngest.getAge())
+                youngest = user;
+        return youngest;
+    };
+
+    public static final UserSelector FIRST_ALPHABETICAL = users -> {
+        User first = null;
+        for (User user : users)
+            if (first == null || user.getFullName().compareTo(first.getFullName()) < 0)
+                first = user;
+        return first;
+    };
+
+    public static final UserSelector LAST_ALPHABETICAL = users -> {
+        User last = null;
+        for (User user : users)
+            if (last == null || user.getFullName().compareTo(last.getFullName()) > 0)
+                last = user;
+        return last;
+    };
+
+
+    /* public static final UserSelector OLDEST = users -> users.stream()
             .max(Comparator.comparingInt(User::getAge))
             .orElse(null);
 
@@ -22,4 +58,6 @@ public class UserSelectors {
     public static final UserSelector LAST_ALPHABETICAL = users -> users.stream()
             .max(Comparator.comparing(User::getFullName))
             .orElse(null);
+
+     */
 }
